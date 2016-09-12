b4b3f7edb1d1666a1656e371ccfeecf7a95d2eb5
H-Plus-Time <nicholas.roberts.au@gmail.com>
2016-09-13 01:15:04 +1000

    Additional work to qr code handling, switched to better maintained lib

--- build.js
+++ build.js
@@ -9,3 +9,7 @@ fs.copy('node_modules/jimp/browser/lib/jimp.min.js', 'js/jimp.min.js', function 
   if (err) return console.error(err)
   console.log("success!")
 });
+fs.copy('node_modules/qrcode-reader/dist/browser.js', 'js/qr.js', function (err) {
+  if (err) return console.error(err)
+  console.log("success!")
+});
