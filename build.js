86a4d5232852a1e3e00ba9d82b7353fa8c39b4f9
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2016-10-20 16:35:31 +1100

    Minor tweaks -last-ditch effort to improve pouchdb responsiveness under heavy load

--- build.js
+++ build.js
@@ -17,3 +17,9 @@ fs.copy('node_modules/geopouch/dist/geopouch.js', 'js/geopouch.js', function(err
   if(err) return console.error(err)
   console.log("success!")
 })
+
+fs.copy('node_modules/worker-pouch/dist/pouchdb.worker-pouch.min.js',
+  'js/worker-pouch.js', function(err) {
+  if(err) return console.error(err)
+  console.log("success!")
+})
